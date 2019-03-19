2a7d02800e48ff871a2d4cf8f4cbbf345451587e
root <root@innotorlife>
2019-03-19 15:50:40 -0400

    Initial commit

---
+++ BoardConfig.mk
@@ -0,0 +1,36 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := device/tcl/5041c
+
+TARGET_BOARD_PLATFORM := U50A_ATT
+TARGET_BOOTLOADER_BOARD_NAME := 5041c
+
+# Recovery
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 11910048
+BOARD_FLASH_BLOCK_SIZE := 0
+BOARD_HAS_NO_REAL_SDCARD := true
+BOARD_SUPPRESS_SECURE_ERASE := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_RECOVERY_SWIPE := true
+BOARD_USES_MMCUTILS := true
+BOARD_SUPPRESS_EMMC_WIPE := true
+TW_INPUT_BLACKLIST := "hbtp_vm"
+TW_INCLUDE_CRYPTO := true
+include $(LOCAL_PATH)/kernel.mk
+include device/generic/twrpbuilder/BoardConfig32.mk
+
